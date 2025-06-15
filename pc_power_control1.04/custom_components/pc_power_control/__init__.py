d80f9966cbdada617b326c0e969aaa2d9b4b6682
Timman70 <timman7035@gmail.com>
2025-06-15 19:05:51 -0400

    Add files via upload

---
+++ pc_power_control1.04/custom_components/pc_power_control/__init__.py
@@ -0,0 +1,8 @@
+from .const import DOMAIN
+
+async def async_setup_entry(hass, config_entry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setups(config_entry, ["switch"])
+    )
+    return True
